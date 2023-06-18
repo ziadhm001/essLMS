523a2983c34cf8cfe0fac9fbed606528f503324f
Ziad Hazem <ziadhazem@Ziads-Mac-mini.local>
2023-06-18 23:06:07 +0300

    Added homepage

---
+++ src/components/LargeCourseCard.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div> 
+            Welcome! you are in home page
+        </div>
+    )
+};
+
+export { Home as default }
